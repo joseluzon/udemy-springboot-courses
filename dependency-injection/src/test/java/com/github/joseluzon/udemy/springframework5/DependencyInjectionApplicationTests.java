2cff2f7612bde020c7be8c1d48f1c70295600029
joseluzon <jose.luzonmartin@gmail.com>
2023-04-02 20:38:30 +0000

    Dependency Injection project

---
+++ dependency-injection/src/test/java/com/github/joseluzon/udemy/springframework5/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joseluzon.udemy.springframework5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
